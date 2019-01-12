7e708782a9d344921f1c23c3823adfd506f6a59f
Steven Jarrett <stevenrjarrett@gmail.com>
2019-01-12 12:38:49 -0700

    Added OPENCV_Sandbox folder as a testing ground for OpenCV

---
+++ OPENCV_Sandbox/build/CMakeFiles/OPENCV_Sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPENCV_Sandbox.dir/main.cpp.o"
+  "OPENCV_Sandbox.pdb"
+  "OPENCV_Sandbox"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPENCV_Sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OPENCV_Sandbox/main.cpp
@@ -0,0 +1,26 @@
+#include "tracking.cpp"
+
+
+
+
+
+//#include <opencv2/opencv.hpp>
+//
+//int main()
+//{
+//    cv::VideoCapture webcam(0);
+//    cv::Mat img;
+//
+//    while(true)
+//    {
+//        if(!webcam.read(img))
+//            break;
+//
+//        cv::imshow("Webcam input", img);
+//
+//        int c = cv::waitKey(30);
+//        if(c == 27) // if ESC is pressed
+//            break;
+//    }
+//    return 0;
+//}//*/

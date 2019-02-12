4f782802d3ea6ea21a869b7aaee780f53908d503
Steven Jarrett <stevenrjarrett@gmail.com>
2019-02-12 13:25:31 -0700

    Added serial parsing for arduino comm's

--- Arduino_Sandbox/Motor_Test/Motor_Test.ino
+++ Arduino_Sandbox/Motor_Test/Motor_Test.ino
@@ -11,21 +11,37 @@ void setup()
   lMotor.attach(8);
   lMotor.writeMicroseconds(1000);
   
-  
 }
 
 
 void loop()
 {
+  
+  // Input value from console
+  int in_val = 0;
+  if (Serial.available()) {
+    int in_val = Serial.parseInt();
+    int in_servoVal = map(in_val, 0, 1023, 1000, 2000);
+    int in_pwmVal  = map(in_val, 0, 1023, 0, 255);
+    lMotor.writeMicroseconds(in_val);
+    analogWrite(5, in_pwmVal);
+    Serial.print("in_val = ");
+    Serial.println(in_val);
+    Serial.print("\n");
+    delay(DELAYTIME);
+  }
+  
+  
+  /* // Read in value from potentiometer
   int val = analogRead(A0);
   int servoVal = map(val, 0, 1023, 1000, 2000);
   int pwmVal  = map(val, 0, 1023, 0, 255);
   lMotor.writeMicroseconds(val);
-  analogWrite(3, pwmVal);
+  analogWrite(5, pwmVal);
   
   Serial.print("pwmVal = ");
   Serial.print(pwmVal);
   Serial.print("\n");
   delay(DELAYTIME);
-  
+  */
 }

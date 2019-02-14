1234b266ec645922ac6e0a584cd420d029d9a211
Steven Jarrett <stevenrjarrett@gmail.com>
2019-02-14 10:42:27 -0700

    Attempting to get brio camera working

--- OPENCV_Sandbox/main.cpp
+++ OPENCV_Sandbox/main.cpp
@@ -1,5 +1,5 @@
 //#include "Matching.cpp"
-#include "Tracking_matching_Dual-camera.cpp"
+//#include "Tracking_matching_Dual-camera.cpp"
 //#include <opencv2/opencv.hpp>
 //
 //
@@ -15,24 +15,48 @@
 
 
 
+#include <unistd.h>
+#include <opencv2/opencv.hpp>
+int device = 1;
 
-//#include <opencv2/opencv.hpp>
+int main()
+{
+        // Do some grabbing
+    cv::VideoCapture video_capture;
+    cv::Mat captured_image;
+    video_capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('M','J','P','G'));
+    video_capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    video_capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    video_capture.set(cv::CAP_PROP_FPS, 30);
+
+    std::cout << "Attempting to capture from device: " << device;
+    video_capture = cv::VideoCapture(device);
+
+    if (!video_capture.isOpened())
+    {
+        std::cout << "Failed to open video source";
+        return 1;
+    }
+
+    std::cout << "Device or file opened";
+    video_capture >> captured_image;
+
+    usleep(10000000);
 //
-//int main()
-//{
 //    cv::VideoCapture webcam(1);
-//    cv::Mat img;
-//
-//    while(true)
-//    {
-//        if(!webcam.read(img))
-//            break;
-//
-//        cv::imshow("Webcam input", img);
-//
-//        int c = cv::waitKey(30);
-//        if(c == 27) // if ESC is pressed
-//            break;
-//    }
-//    return 0;
-//}//*/
+    cv::Mat img;
+
+    while(true)
+    {
+//    if(!video_capture.read(img))
+//        break;
+        video_capture >> captured_image;
+
+        cv::imshow("Webcam input", captured_image);
+
+        int c = cv::waitKey(30);
+        if(c == 27) // if ESC is pressed
+            break;
+    }
+    return 0;
+}//*/

f0bb1be82a424e3e65a857e9031b2a5b77d7e150
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-02 14:36:44 -0600

    Working on detectnet-camera-opencv

--- detectnet-camera/detectnet-camera-opencv.cpp
+++ detectnet-camera/detectnet-camera-opencv.cpp
@@ -201,18 +201,18 @@ int main( int argc, char** argv )
 		// convert from YUV to RGBA and move to graphics memory
 
         unsigned long numElem = rgbimg.total()*4;
-        uchar* camData = new uchar[numElem];
+//        uchar* camData = new uchar[numElem];
         float* camDataflt;
         cudaMallocManaged(&camDataflt, sizeof(float)*numElem);
-        cv::Mat continuousRGBA(rgbimg.size(), CV_8UC4, camData);
+        cv::Mat continuousRGBA(rgbimg.size(), CV_32FC4, camDataflt);
         cv::cvtColor(rgbimg, continuousRGBA, CV_BGR2RGBA, 4);
-        for(int i=0; i<numElem; i+=4)
-        {
-            camDataflt[i]   = (float)camData[i];
-            camDataflt[i+1] = (float)camData[i+1];
-            camDataflt[i+2] = (float)camData[i+2];
-            camDataflt[i+3] = (float)camData[i+3];
-        }
+//        for(int i=0; i<numElem; i+=4)
+//        {
+//            camDataflt[i]   = (float)camData[i];
+//            camDataflt[i+1] = (float)camData[i+1];
+//            camDataflt[i+2] = (float)camData[i+2];
+//            camDataflt[i+3] = (float)camData[i+3];
+//        }
 
 //		void* imgRGBA = NULL;
 //

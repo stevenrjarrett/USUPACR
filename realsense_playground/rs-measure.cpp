52b8f1db6ad5ffdbfd141d758724473d6e5f119a
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-02 21:16:07 -0600

    Working on realsense-playground

--- realsense_playground/rs-measure.cpp
+++ realsense_playground/rs-measure.cpp
@@ -1,19 +1,68 @@
+//
+//// License: Apache 2.0. See LICENSE file in root directory.
+//// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+//
+//#include <librealsense2/rs.hpp>
+//#include <librealsense2/hpp/rs_internal.hpp>
+//#include <iostream>
+//
+//int main()
+//{
+//    rs2::context ctx;
+//
+//    std::cout << "hello from librealsense - " << RS2_API_VERSION_STR << std::endl;
+//    std::cout << "You have " << ctx.query_devices().size() << " RealSense devices connected" << std::endl;
+//
+//
+//
+//    return 0;
+//}
+
 
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 
-#include <librealsense2/rs.hpp>
-#include <librealsense2/hpp/rs_internal.hpp>
-#include <iostream>
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include <iostream>             // for cout
 
-int main()
+// Hello RealSense example demonstrates the basics of connecting to a RealSense device
+// and taking advantage of depth data
+int main(int argc, char * argv[]) try
 {
-    rs2::context ctx;
+    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
+    rs2::pipeline p;
 
-    std::cout << "hello from librealsense - " << RS2_API_VERSION_STR << std::endl;
-    std::cout << "You have " << ctx.query_devices().size() << " RealSense devices connected" << std::endl;
+    // Configure and start the pipeline
+    p.start();
 
+    while (true)
+    {
+        // Block program until frames arrive
+        rs2::frameset frames = p.wait_for_frames();
 
+        // Try to get a frame of a depth image
+        rs2::depth_frame depth = frames.get_depth_frame();
 
-    return 0;
+        // Get the depth frame's dimensions
+        float width = depth.get_width();
+        float height = depth.get_height();
+
+        // Query the distance from the camera to the object in the center of the image
+        float dist_to_center = depth.get_distance(width / 2, height / 2);
+
+        // Print the distance
+        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
 }

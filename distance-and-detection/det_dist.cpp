2c5589dbf0546291950f161fc9f8fa3a5ad281ed
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-06 10:02:39 -0600

    Working on distance-and-detection

--- distance-and-detection/det_dist.cpp
+++ distance-and-detection/det_dist.cpp
@@ -365,7 +365,7 @@ int main(int argc, char * argv[]) try
 
                     printf("bw box       %i  (%f, %f)  w=%f  h=%f\n", n, drect.x, drect.y, drect.width, drect.height);
                     cv::rectangle(colorMat, crect, cv::Scalar( 255, 0, 0 ), 2, 1 );
-                    cv::rectangle(depthMat, drect, cv::Scalar( 255, 255, 255 ), 2, 1 );
+                    cv::rectangle(depthMat, drect, cv::Scalar( 1, 1, 1 ), 2, 1 );
 
 				if( nc != lastClass || n == (numBoundingBoxes - 1) )
 				{

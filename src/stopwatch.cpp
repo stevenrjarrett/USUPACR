385988d4f90d18615dc837c80c1d33391cb290b5
Steven Jarrett <A01632637@aggies.usu.edu>
2019-04-09 11:25:27 -0600

    working on pacr-follow

--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -1,29 +1,31 @@
-#include <stopwatch.h>
+#include "stopwatch.h"
 //#include <chrono>
 
-class stopwatch
+
+stopwatch::stopwatch()
 {
-    std::chrono::time_point<std::chrono::high_resolution_clock> time_reference;
-    public:
-    stopwatch()
-    {
-        reset();
-    }
-    void reset()
-    {
-        time_reference = std::chrono::high_resolution_clock::now();
-    }
-    double seconds()
-    {
-        auto d = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-time_reference);
-        double elapsed = static_cast<double>(d.count())/1000000000;
-        return elapsed;
-    }
-    long long abs_time_LongLong()
-    {
-        auto t = std::chrono::high_resolution_clock::now().time_since_epoch();
-        auto d = std::chrono::duration_cast<std::chrono::nanoseconds>(t);
-        long long elapsed = d.count();
-        return elapsed;
-    }
-};
+    reset();
+}
+
+// resets the stopwatch (saves the time reference as right now.)
+void stopwatch::reset()
+{
+    time_reference = std::chrono::high_resolution_clock::now();
+}
+
+// returns the number of seconds since the reference time
+double stopwatch::seconds()
+{
+    auto d = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-time_reference);
+    double elapsed = static_cast<double>(d.count())/1000000000;
+    return elapsed;
+}
+
+// returns the time since epoch
+long long stopwatch::abs_time_LongLong()
+{
+    auto t = std::chrono::high_resolution_clock::now().time_since_epoch();
+    auto d = std::chrono::duration_cast<std::chrono::nanoseconds>(t);
+    long long elapsed = d.count();
+    return elapsed;
+}

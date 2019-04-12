076b2cf9946170fb61856d31bc35b95c2be41903
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-12 10:35:41 -0600

    Working on follow-demo

---
+++ GUI/mainwindow.h
@@ -0,0 +1,31 @@
+//*******Header*******
+
+ #ifndef MAINWINDOW_H
+ #define MAINWINDOW_H
+
+ //#include <QMainWindow>
+ //#include <QPushButton>
+ //#include <QRadioButton>
+ //#include <QGroupBox>
+
+ #include <QtWidget>
+
+ class QGroupBox;
+
+ class MainWindow : public QWidget
+ {
+    Q_OBJECT
+
+ public:
+    MainWindow(QWidget *parent = 0);
+
+ private slots:
+    void handleButton();
+
+ private:
+    QGroupBox *createDistanceGroup();
+    QGroupBox *createSpeedGroup();
+    QGroupBox *createAutonomousGroup();
+ };
+
+ #endif

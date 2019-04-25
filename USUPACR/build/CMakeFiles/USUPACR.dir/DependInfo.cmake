ec83c33def13154f7e9010011d3855a21d6dc597
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-25 16:48:26 -0600

    Working on USUPACR

--- USUPACR/build/CMakeFiles/USUPACR.dir/DependInfo.cmake
+++ USUPACR/build/CMakeFiles/USUPACR.dir/DependInfo.cmake
@@ -1,9 +1,44 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nvidia/Documents/USUPACR/src/libenjoy.c" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/libenjoy.c.o"
+  "/home/nvidia/Documents/USUPACR/src/libenjoy_linux.c" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/libenjoy_linux.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../../include"
+  ".././include"
+  "../."
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/local/cuda-9.0/include"
+  "/usr/share/qt4/mkspecs/default"
+  "/usr/local/include"
+  "/usr/include/opencv"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/Documents/USUPACR/USUPACR/build/__/include/moc_mainwindow.cxx" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/__/include/moc_mainwindow.cxx.o"
+  "/home/nvidia/Documents/USUPACR/src/XBoxOne.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/XBoxOne.cpp.o"
+  "/home/nvidia/Documents/USUPACR/src/cameraDetection.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/cameraDetection.cpp.o"
+  "/home/nvidia/Documents/USUPACR/src/mainwindow.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/mainwindow.cpp.o"
+  "/home/nvidia/Documents/USUPACR/src/motorController.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/motorController.cpp.o"
+  "/home/nvidia/Documents/USUPACR/src/personTracker.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/personTracker.cpp.o"
+  "/home/nvidia/Documents/USUPACR/src/stopwatch.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/stopwatch.cpp.o"
   "/home/nvidia/Documents/USUPACR/USUPACR/main.cpp" "/home/nvidia/Documents/USUPACR/USUPACR/build/CMakeFiles/USUPACR.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
@@ -17,16 +52,17 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../include"
+  ".././include"
+  "../."
   "/usr/include/qt4"
   "/usr/include/qt4/QtGui"
   "/usr/include/qt4/QtCore"
   "/usr/local/cuda-9.0/include"
-  "/usr/include/gstreamer-1.0"
-  "/usr/lib/aarch64-linux-gnu/gstreamer-1.0/include"
-  "/usr/include/glib-2.0"
-  "/usr/include/libxml2"
-  "/usr/lib/aarch64-linux-gnu/glib-2.0/include"
   "/usr/share/qt4/mkspecs/default"
+  "/usr/local/include"
+  "/usr/include/opencv"
   )
 
 # Targets to which this target links.
--- USUPACR/build/CMakeFiles/USUPACR.dir/cmake_clean.cmake
+++ USUPACR/build/CMakeFiles/USUPACR.dir/cmake_clean.cmake
@@ -1,10 +1,20 @@
 file(REMOVE_RECURSE
+  "__/include/moc_mainwindow.cxx"
   "CMakeFiles/USUPACR.dir/main.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/motorController.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/cameraDetection.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/XBoxOne.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/personTracker.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/mainwindow.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/stopwatch.cpp.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/libenjoy_linux.c.o"
+  "CMakeFiles/USUPACR.dir/home/nvidia/Documents/USUPACR/src/libenjoy.c.o"
+  "CMakeFiles/USUPACR.dir/__/include/moc_mainwindow.cxx.o"
   "USUPACR.pdb"
   "USUPACR"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/USUPACR.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()

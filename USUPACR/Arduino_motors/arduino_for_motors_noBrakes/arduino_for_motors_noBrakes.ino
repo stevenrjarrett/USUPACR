160ee6de3fbd45d9079fc01b7ff56eba93863352
Steven Jarrett <stevenrjarrett@gmail.com>
2019-04-26 16:39:00 -0600

    Trying to get the motor arduino to be more reliable

--- USUPACR/Arduino_motors/arduino_for_motors_noBrakes/arduino_for_motors_noBrakes.ino
+++ USUPACR/Arduino_motors/arduino_for_motors_noBrakes/arduino_for_motors_noBrakes.ino
@@ -20,7 +20,7 @@ int sign(int val) { return val==0 ? 1 : val / abs(val);}
 
 void setup()
 {
-  Serial.begin(1000000);
+  Serial.begin(19200);
 //  Serial.println("Hi");
   pinMode(LMOTOR, OUTPUT);
   pinMode(RMOTOR, OUTPUT);
